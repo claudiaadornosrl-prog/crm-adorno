537fbcdb6289cb654beb1def2e26c88f3d25759d
Claudia Adorno <claudiaadornosrl@gmail.com>
2026-05-29 00:53:21 -0300

    chore: limpiar .gitignore y untrack archivos basura (no borra del disco)

--- _setup_verif_encuestas.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-schtasks /create /tn "CRM_Adorno_SyncEncuestas" /tr "python C:\CRM_Adorno\rrhh-adorno\sync_anviz\sync_encuestas.py --aplicar" /sc daily /mo 2 /st 08:00 /ru "%USERDOMAIN%\%USERNAME%" /it /f > C:\CRM_Adorno\_verif_tarea.txt 2>&1
-echo ----- QUERY ----- >> C:\CRM_Adorno\_verif_tarea.txt
-schtasks /query /tn "CRM_Adorno_SyncEncuestas" /fo LIST /v >> C:\CRM_Adorno\_verif_tarea.txt 2>&1
